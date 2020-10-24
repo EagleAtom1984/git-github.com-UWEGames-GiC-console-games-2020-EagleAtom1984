24747fdbfa8f6e0469d13dcebb4fd7dd3ec71779
EagleAtom1984 <72929406+EagleAtom1984@users.noreply.github.com>
2020-10-24 14:09:14 +0100

    Add files via upload

---
+++ C++ Module/github-classroom-practice-EagleAtom1984/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_8141f.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_8141f.dir/CMakeCCompilerABI.c.obj"
+  "cmTC_8141f.exe"
+  "cmTC_8141f.exe.manifest"
+  "cmTC_8141f.pdb"
+  "libcmTC_8141f.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_8141f.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
